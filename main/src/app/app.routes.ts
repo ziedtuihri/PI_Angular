83728e6e4b263b8f9ad938be395b0029e0adbd10
Anwer Hriz <anwer.hriz@esprit.tn>
2025-04-07 01:22:24 +0100

    implemented offer module routes

--- main/src/app/app.routes.ts
+++ main/src/app/app.routes.ts
@@ -3,52 +3,25 @@ import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
 
 import { AuthGuard } from './services/auth.guard';
+import { EntryComponent } from './esprit/entry/entry.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: FullComponent,
-
-    children: [
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full',
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./pages/pages.routes').then((m) => m.PagesRoutes),
-      },
-      {
-        path: 'ui-components',
-        loadChildren: () =>
-          import('./pages/ui-components/ui-components.routes').then(
-            (m) => m.UiComponentsRoutes
-          ),
-      },
-      {
-        path: 'extra',
-        loadChildren: () =>
-          import('./pages/extra/extra.routes').then((m) => m.ExtraRoutes),
-      },
-    ],
+    component: EntryComponent, 
   },
   {
-    path: '',
-    component: BlankComponent,
-    children: [
-      {
-        path: 'authentication',
-        loadChildren: () =>
-          import('./pages/authentication/authentication.routes').then(
-            (m) => m.AuthenticationRoutes
-          ),
-      },
-    ],
+    path: 'frontoffice',
+    loadChildren: () =>
+      import('./esprit/frontoffice/frontoffice.module').then((m) => m.FrontofficeModule),
+  },
+  {
+    path: 'backoffice',
+    loadChildren: () =>
+      import('./esprit/backoffice/backoffice.module').then((m) => m.BackofficeModule),
   },
   {
     path: '**',
-    redirectTo: 'authentication/error',
+    redirectTo: '',
   },
 ];
