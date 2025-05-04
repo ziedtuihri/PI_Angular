7d3b2dcff16780d589841a124792f33bc7d1644b
Zied Touahri <35895902+ziedtuihri@users.noreply.github.com>
2025-05-04 12:10:58 +0100

    delete all files

--- main/src/app/pages/extra/icons/icons.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { MaterialModule } from '../../../material.module';
-
-@Component({
-  selector: 'app-icons',
-  imports: [MaterialModule],
-  templateUrl: './icons.component.html',
-})
-export class AppIconsComponent { }
